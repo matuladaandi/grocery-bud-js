18a594630b54edc1fc088990a9f6c27563d40558
matuladaandi@gmail.com <matuladaandi@gmail.com>
2024-04-28 22:09:22 +0800

    set back to default/clear input form

--- app.js
+++ app.js
@@ -1,11 +1,87 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener("submit", addItem);
+
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+
+  if (value && !editFlag) {
+    // ** if YES value
+    const element = document.createElement("article");
+
+    // ** add class
+    element.classList.add("grocery-item");
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `
+    <p class="title">${value}</p>
+    <div class="btn-container">
+      <button class="edit-btn" type="button"><i class="fas fa-edit"></i></button>
+      <button class="delete-btn" type="button"><i class="fas fa-trash"></i></button>
+    </div>
+    `;
+
+    // ** append child
+    list.appendChild(element);
+    // display alert
+    displayAlert("item add to the list", "success");
+    // show container
+    container.classList.add("show-container");
+    // ** add to local storage
+    addToLocalStorage(id, value);
+    // set back to default
+    setBackToDefault();
+  } else if (value && editFlag) {
+    // *TODO if edit
+    console.log("editing");
+  } else {
+    // *! if NOT ada value
+    displayAlert("please enter value", "danger");
+  }
+}
+
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  // remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1200);
+}
+
+// set back to default
+function setBackToDefault() {
+  grocery.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.textContent = "submit";
+}
 
 // ****** FUNCTIONS **********
 
 // ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value) {
+  console.log("add local");
+}
 
 // ****** SETUP ITEMS **********
